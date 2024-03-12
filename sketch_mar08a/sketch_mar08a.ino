1e91f90e44cbeb016ffbb7d395364529fb449a98
alextassy24 <t_alex_00@yahoo.com>
2024-03-12 15:52:11 +0200

    small changes

--- sketch_mar08a/sketch_mar08a.ino
+++ sketch_mar08a/sketch_mar08a.ino
@@ -4,14 +4,13 @@
 
 TinyGPSPlus gps;
 TinyGPSPlus lastGps;
-SoftwareSerial mygps(8, 9);
+SoftwareSerial mygps(4, 3); // tx-4, rx-3
 #define rxPin 0
 #define txPin 1
 SoftwareSerial sim800L(rxPin, txPin);
 
 ezButton toggleSwitch(7);
 bool switchStatePrev = false;
-bool gpsDataAvailable = false;
 
 void setup() {
   Serial.begin(9600);
@@ -49,10 +48,12 @@ void setup() {
     if (sim800L.find("OK")) {
       Serial.println("GPRS enabled!");
       Serial.println("Setup Complete!");
-    } else {
+    }
+    else {
       Serial.println("Failed to enable GPRS connection!");
     }
-  } else {
+  }
+  else {
     Serial.println("SIM800L initialization failed!");
   }
 }
@@ -63,60 +64,38 @@ void loop() {
   while (sim800L.available()) {
     Serial.println(sim800L.readString());
   }
-  while (Serial.available())  {
+
+  while (Serial.available()) {
     sim800L.println(Serial.readString());
   }
 
-  int state = toggleSwitch.getState();
-  
-  if (state != switchStatePrev) {
-    switchStatePrev = state;
-    if (state == HIGH) {
-      Serial.println("The switch is ON");
-    }
-    else {
-      Serial.println("The switch is OFF");
-    }
-    delay(1000);
+  if (toggleSwitch.getState() != switchStatePrev) {
+    switchStatePrev = toggleSwitch.getState();
+    Serial.println(switchStatePrev ? "The switch is ON" : "The switch is OFF");
   }
 
-  if (state == HIGH) {
-    if (mygps.available()) {
-      while (mygps.available()) {
+  if (toggleSwitch.getState() == HIGH) {
+    if (mygps.available() > 0) {
+      while (mygps.available() > 0) {
+        gps.encode(mygps.read());
         lastGps = gps;
-        char c = mygps.read();
-        gps.encode(c);
-      }
 
-      if (gps.location.isUpdated()) {
-        Serial.println("GPS location updated");
+        if (gps.location.isUpdated()) {
+          Serial.println("GPS location updated");
+        }
+        else if (gps.location.lat() == lastGps.location.lat() &&
+                 gps.location.lng() == lastGps.location.lng()) {
+          Serial.println("Same position");
+        }
         Serial.print("Latitude= ");
         Serial.print(gps.location.lat(), 6);
-        Serial.print(" Longitude= ");
+        Serial.print("Longitude= ");
         Serial.println(gps.location.lng(), 6);
-        gpsDataAvailable = true;
-      }
-      else if (gps.location.lat() == lastGps.location.lat() &&
-               gps.location.lng() == lastGps.location.lng() &&
-               gps.location.lat() != 0 &&
-               gps.location.lng() != 0) {
-        if (!gpsDataAvailable) {
-          gpsDataAvailable = true;
-          Serial.print("Same position");
-          Serial.print("Latitude= ");
-          Serial.print(lastGps.location.lat(), 6);
-          Serial.print("Longitude= ");
-          Serial.println(lastGps.location.lng(), 6);
-        }
       }
     }
     else {
-      if (gpsDataAvailable) {
-        gpsDataAvailable = false;
-        Serial.println("No data available from GPS module");
-      }
+      Serial.println("No data available from GPS module");
     }
   }
-
-  delay(1000); // Delay between loop iterations
+  delay(1000);
 }
