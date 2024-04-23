da6505debadb324f450d3fd939dd1e5e450357db
alextanaseici <alexandru.tanase@ici.ro>
2024-04-23 15:57:43 +0300

    Modified gsm

--- test_gsm/test_gsm_1.ino-FUNCTIONAL/test_gsm_1.ino-FUNCTIONAL.ino
+++ test_gsm/test_gsm_1.ino-FUNCTIONAL/test_gsm_1.ino-FUNCTIONAL.ino
@@ -1,101 +1,122 @@
+
+
+#define TINY_GSM_MODEM_SIM800
+
+#include <TinyGsmClient.h>
+#include <ArduinoHttpClient.h>
 #include <SoftwareSerial.h>
 
-//Create software serial object to communicate with SIM800L
-SoftwareSerial mySerial(3, 2); //SIM800L Tx & Rx is connected to Arduino #3 & #2
+SoftwareSerial mySerial(3, 2); // SIM800L Tx & Rx Arduino #3 & #2
+// portocaliu pin3, maro pin2
 
-void setup()
-{
-  //Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
+bool connectionEstablished = false;
+
+char apn[]  = "internet";
+char user[] = "";
+char pass[] = "";
+
+const char server[]   = "disertatie-api.azurewebsites.net";
+const int  port       = 80;
+
+TinyGsm modem(mySerial);
+TinyGsmClient client(modem);
+HttpClient http(modem, server, port);
+
+
+void setup() {
   Serial.begin(9600);
-  //Begin serial communication with Arduino and SIM800L
   mySerial.begin(9600);
-  Serial.println("");
-  establishConnection();
-  delay(1000);
-  Serial.println("Initializing complete!");
-
-  //  mySerial.println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
-  //  updateSerial();
-  //  mySerial.println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
-  //  updateSerial();
-  //  mySerial.println("ATi"); //Returns the module name and revision
-  //  updateSerial();
-  //  mySerial.println("AT+COPS=?"); //Returns the list of operators present in the network
-  //  updateSerial();
-  //  mySerial.println("AT+CBC"); //Returns Li-Po battery status. The second number is the battery level (in our case it is 93%)
-  //  //and the third number is the actual voltage in mV
-  //  updateSerial();
-  // Configure GPRS connection
-  //  mySerial.println("AT+SAPBR=3,1,\"APN\",\"internet.vodafone.ro\"");
-  //  updateSerial();
-}
-
-void loop()
-{
-  delay(1000);
-  sendData();
-}
-
-void updateSerial()
-{
-  delay(500);
-  while (Serial.available())
-  {
-    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
-  }
-  while (mySerial.available())
-  {
-    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+  //  Serial.println("INFO: Trying to connect to the network...");
+  //  connectionEstablished = establishConnection();
+  //  if (connectionEstablished) {
+  //    Serial.println("INFO: Initializing complete!");
+  //  } else {
+  //    Serial.println("ERROR: Connection establishment failed.");
+  //    while (true) {
+  //      while (Serial.available())
+  //      {
+  //        mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+  //      }
+  //      while (mySerial.available())
+  //      {
+  //        Serial.write(mySerial.read())s;//Forward what Software Serial received to Serial Port
+  //      }
+  //    }
+  //  }
+  Serial.println("Initializing modem...");
+  modem.restart();
+  Serial.println("Restart Over...........");
+  String modemInfo = modem.getModemInfo();
+  Serial.print("Modem: ");
+  Serial.println(modemInfo);
+  modem.init();
+  if (modem.isGprsConnected()) {
+    Serial.println("GPRS connected");
   }
 }
 
-void establishConnection() {
-  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
-  updateSerial();
-  mySerial.println("AT+CREG?"); //Check whether it has registered in the network
-  updateSerial();
-  mySerial.println("AT+COPS?"); //Checks which network you are connected to
-  updateSerial();
-  mySerial.println("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"");
-  updateSerial();
-  mySerial.println("AT+SAPBR=3,1,\"APN\",\"internet\"");
-  updateSerial();
-  mySerial.println("AT+SAPBR=1,1");
-  updateSerial();
-  mySerial.println("AT+SAPBR=2,1");
-  updateSerial();
+void loop() {
+  if (connectionEstablished) {
+    static unsigned long lastSendTime = 0;
+    unsigned long currentTime = millis();
+
+    // Send data every 1000 milliseconds
+    if (currentTime - lastSendTime >= 1000) {
+      sendData();
+      lastSendTime = currentTime;
+    }
+  }
 }
+
+bool establishConnection() {
+  if (!sendCommand("AT")) return false;
+  if (!sendCommand("AT+CSQ")) return false;
+  if (!sendCommand("AT+CREG?")) return false;
+  if (!sendCommand("AT+COPS=1,2, \"22605\"")) return false;
+  delay(5000);
+  if (!sendCommand("AT+CREG?")) return false;
+  if (!sendCommand("AT+CGREG?")) return false;
+  if (!sendCommand("AT+SAPBR=2,1")) return false;
+  if (!sendCommand("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"")) return false;
+  if (!sendCommand("AT+SAPBR=3,1,\"APN\",\"internet\"")) return false;
+  if (!sendCommand("AT+SAPBR=3,1,\"USER\",\"\"")) return false;
+  if (!sendCommand("AT+SAPBR=3,1,\"PWD\",\"\"")) return false;
+  if (!sendCommand("AT+SAPBR=4,1")) return false;
+  if (!sendCommand("AT+SAPBR=2,1")) return false;
+  //  if (!sendCommand("AT+SAPBR=0,1")) return false;
+  if (!sendCommand("AT+SAPBR=1,1")) return false;
+  return true;
+}
+
 void sendData() {
-  mySerial.println("AT+HTTPINIT");
-  updateSerial();
-  mySerial.println("AT+HTTPSSL=0");
-  updateSerial();
-  mySerial.println("AT+HTTPPARA=\"CID\",1");
-  updateSerial();
+  sendCommand("AT+HTTPINIT");
+  sendCommand("AT+HTTPSSL=0");
+  sendCommand("AT+HTTPPARA=\"CID\",1");
 
   // Send HTTP GET request
-  String apiURL = "https://disertatie-api.azurewebsites.net/t";
-  // Send AT command to set URL
-  mySerial.print("AT+HTTPPARA=\"URL\",\"");
-  mySerial.print(apiURL);
-  mySerial.println("\"");
+  //  String apiURL = "https://disertatie-api.azurewebsites.net/t";
+  String apiURL = "http://minimalsites.com/";
+  sendCommand("AT+HTTPPARA=\"URL\",\"" + apiURL + "\"");
   delay(100); // Add a small delay to ensure the command is processed properly
-  updateSerial();
 
-  // Wait for response before sending HTTPACTION command
-  delay(500); // Adjust delay as needed
-  mySerial.println("AT+HTTPACTION=0");
+  sendCommand("AT+HTTPACTION=0");
   delay(1000); // Wait for response
-  updateSerial();
 
-  // Read HTTP response
-  mySerial.println("AT+HTTPREAD");
+  sendCommand("AT+HTTPREAD");
   delay(1000); // Wait for response
-  updateSerial();
 
-  // Terminate HTTP session
-  mySerial.println("AT+HTTPTERM");
-  delay(100); // Add a small delay before sending the next command
+  sendCommand("AT+HTTPTERM");
+}
 
-  Serial.println("Data sent successfully");
+bool sendCommand(String command) {
+  mySerial.println(command);
+  delay(500); // Adjust delay if needed
+  while (mySerial.available()) {
+    String response = mySerial.readStringUntil('\n');
+    Serial.println(response);
+    if (response.indexOf("ERROR") != -1) {
+      return false;
+    }
+  }
+  return true;
 }
